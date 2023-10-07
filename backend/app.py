c67db1a805db70cb8dc68d5b8d2d92ef81f1e274
RynnR-zehua-ren <2546644819@qq.com>
2023-10-08 01:05:58 +1100

    remove util file, and add database model to the db folder, connect config to database file

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 from config import app
 
 if __name__ == '__main__':
-    print('Starting the backend, keep it running on this terminal...')
-
     # run on the 9998 port
     app.run(debug=True, port=9998)
 
--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 # create the api
 api = Api(
@@ -12,11 +12,20 @@ api = Api(
     validate=True
 )
 
-# no database for now
-
 # create the app
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# connect to database: ./database.db
+# check if the file ./database.db exist, if not, exit the whole program
+if not os.path.exists('./database.db'):
+    print('Error: database.db not found!')
+    print('Copy the backend/db/default_database.db to the backend folder, and rename it to database.db')
+    exit(1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # wrap with CORS
 CORS(app)
