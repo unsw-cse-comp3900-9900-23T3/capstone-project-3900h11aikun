83be27b625d710cf951a86035da0427591d64cb6
wang957525 <wang957525@qq.com>
2023-10-15 17:47:47 +0800

    add project interface for partner

--- backend/apis/profile.py
+++ backend/apis/profile.py
@@ -1,5 +1,7 @@
 from flask import request, abort, jsonify
 from flask_restx import Resource, Namespace
+from models.profile import *
+from db.models import *
 
 api = Namespace(
     "profile",
@@ -11,3 +13,112 @@ api = Namespace(
 class Login(Resource):
     def get(self):
         pass
+
+
+@api.route("/project")
+class Projects(Resource):
+    @api.doc(description="Create new project")
+    @api.expect(project_create_model)
+    @api.response(200, 'create success')
+    @api.response(400, 'invalid params')
+    @api.response(401, 'only partner can create project')
+    def post(self):
+        data = request.get_json()
+
+        if Partner.query.filter_by(partner_id=data["partner_id"]).first() is None:
+            abort(401, 'unknown partner id')
+        new_project = Project(
+            partner_id=data['partner_id'],
+            title=data['title'],
+            description=data['description'],
+            problem_statement=data['problem_statement'],
+            desired_outcomes=data['desired_outcomes'],
+            required_skills=data['required_skills'],
+            deliverables=data['deliverables'],
+            project_last_updated_at=datetime.now(),
+            status='is_open',
+        )
+        db.session.add(new_project)
+        db.session.commit()
+
+        return jsonify(new_project.as_dict())
+
+    @api.doc(description="Update project detail information")
+    @api.expect(project_update_model)
+    @api.response(200, 'update success')
+    @api.response(400, 'invalid params')
+    @api.response(401, 'only owner partner of project can update project')
+    def put(self):
+        data = request.get_json()
+
+        project = Project.query.filter_by(project_id=data["project_id"], partner_id=data["partner_id"]).first()
+        if project is None:
+            abort(401, 'unknown project id or partner id is not owner')
+        if 'title' in data:
+            project.title = data['title']
+        if 'description' in data:
+            project.description = data['description']
+        if 'problem_statement' in data:
+            project.problem_statement = data['problem_statement']
+        if 'desired_outcomes' in data:
+            project.desired_outcomes = data['desired_outcomes']
+        if 'required_skills' in data:
+            project.required_skills = data['required_skills']
+        if 'deliverables' in data:
+            project.deliverables = data['deliverables']
+        project.project_last_updated_at = datetime.now()
+
+        db.session.commit()
+
+        return jsonify(project.as_dict())
+
+
+@api.route("/project/assign/student")
+class ProjectAssignStudent(Resource):
+    @api.doc(description="Assign student for project")
+    @api.expect(project_assign_student_model)
+    @api.response(200, 'assign success')
+    @api.response(400, 'invalid params')
+    @api.response(401, 'only owner partner of project can update project')
+    def post(self):
+        data = request.get_json()
+
+        project = Project.query.filter_by(project_id=data["project_id"], partner_id=data["partner_id"]).first()
+        if project is None:
+            abort(401, 'unknown project id or partner id is not owner')
+        student = Student.query.filter_by(student_id=data["student_id"]).first()
+        if student is None:
+            abort(401, 'unknown student id')
+
+        project.student_id = student.student_id
+        project.student_being_assigned_at = datetime.now()
+
+        db.session.commit()
+
+        return jsonify(project.as_dict())
+
+
+@api.route("/project/assign/supervisor")
+class ProjectAssignSupervisor(Resource):
+    @api.doc(description="Assign supervisor for project")
+    @api.expect(project_assign_supervisor_model)
+    @api.response(200, 'assign success')
+    @api.response(400, 'invalid params')
+    @api.response(401, 'only owner partner of project can update project')
+    def post(self):
+        data = request.get_json()
+
+        project = Project.query.filter_by(project_id=data["project_id"], partner_id=data["partner_id"]).first()
+        if project is None:
+            abort(401, 'unknown project id or partner id is not owner')
+        supervisor = Supervisor.query.filter_by(supervisor_id=data["supervisor_id"]).first()
+        if supervisor is None:
+            abort(401, 'unknown supervisor id')
+
+        project.supervisor_id = supervisor.supervisor_id
+        project.supervisor_being_assigned_at = datetime.now()
+
+        db.session.commit()
+
+        return jsonify(project.as_dict())
+
---
+++ backend/models/profile.py
@@ -0,0 +1,46 @@
+from flask_restx import fields, reqparse
+from config import api
+from werkzeug.datastructures import FileStorage
+
+
+# project create requires project id, partner id and the detail information of project
+project_create_model = api.model('project_create', {
+    'partner_id': fields.Integer(required=True, description='partner id of project owner'),
+    'title': fields.String(required=True, description='title of project'),
+    'description': fields.String(required=True, description='description of project'),
+    'problem_statement': fields.String(required=True, description='problem statement of project'),
+    'desired_outcomes': fields.String(required=True, description='desired outcomes of project'),
+    'required_skills': fields.String(required=True, description='required skills of project'),
+    'deliverables': fields.String(required=True, description='deliverables of project'),
+})
+
+
+# project update requires project id and partner id of the project owner
+# only the owner can update project information
+project_update_model = api.model('project_update', {
+    'project_id': fields.Integer(required=True, description='project id'),
+    'partner_id': fields.Integer(required=True, description='partner id of project owner'),
+    'title': fields.String(required=False, description='title of project'),
+    'description': fields.String(required=False, description='description of project'),
+    'problem_statement': fields.String(required=False, description='problem statement of project'),
+    'desired_outcomes': fields.String(required=False, description='desired outcomes of project'),
+    'required_skills': fields.String(required=False, description='required skills of project'),
+    'deliverables': fields.String(required=False, description='deliverables of project'),
+    'status': fields.String(required=False, description='status of project'),
+})
+
+
+# project assign student requires project id, partner id of the project owner and student id to be assigned
+project_assign_student_model = api.model('project_assign_student', {
+    'project_id': fields.Integer(required=True, description='project id'),
+    'partner_id': fields.Integer(required=True, description='partner id of project owner'),
+    'student_id': fields.Integer(required=True, description='student id to be assigned'),
+})
+
+
+# project assign student requires project id, partner id of the project owner and supervisor id to be assigned
+project_assign_supervisor_model = api.model('project_assign_student', {
+    'project_id': fields.Integer(required=True, description='project id'),
+    'partner_id': fields.Integer(required=True, description='partner id of project owner'),
+    'supervisor_id': fields.Integer(required=True, description='supervisor id to be assigned'),
+})
