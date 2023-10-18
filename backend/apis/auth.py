fbbaecc2cc9e6c36085e48e894137a55ed2e6ead
RynnR-zehua-ren <2546644819@qq.com>
2023-10-18 16:52:01 +1100

    add two auth routes about return token

--- backend/apis/auth.py
+++ backend/apis/auth.py
@@ -11,37 +11,53 @@ from config import app
 import time
 
 
-# check the attributes of the request
-def check_student_register_info(data):
-    # check if any attribtues are empty
-    for key, value in data.items():
-        if value == "":
-            abort(400, "empty value at {}".format(key))
+# define the api
+api = Namespace(
+    "auth",
+    description="login, register"
+)
 
-    # check if the username is unique
-    is_username_duplicate = Student.query.filter_by(username=data["username"]).first()
-    if is_username_duplicate is not None:
-        abort(400, "username is duplicate")
 
-    # check if the email is unique
-    is_email_duplicate = Student.query.filter_by(email=data["email"]).first()
-    if is_email_duplicate is not None:
-        abort(400, "email is duplicate")
+# a combination of login with either username or email,
+# and choose to return token of not
+def login(username=None, email=None, password=None, type=None, return_token=False):
+    if email is None:
+        # use username and password
+        if type == "student":
+            result_user = Student.query.filter_by(username=username, password=password).first()
+        elif type == "supervisor":
+            result_user = Supervisor.query.filter_by(username=username, password=password).first()
+        else:
+            result_user = Partner.query.filter_by(username=username, password=password).first()
+    else:
+        # use email and password
+        if type =="student":
+            result_user = Student.query.filter_by(email=email, password=password).first()
+        elif type == "supervisor":
+            result_user = Supervisor.query.filter_by(email=email, password=password).first()
+        else:
+            result_user = Partner.query.filter_by(email=email, password=password).first()
 
-    # password needs to be 8 characters long, one uppercase, one lowercase, one number
-    if len(data["password"]) < 8:
-        abort(400, "password needs to be at least 8 characters long")
+    if result_user is None:
+        return None
 
-    # password pattern: ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$
-    # and allow special characters
-    password_pattern = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")
-    if not password_pattern.match(data["password"]):
-        abort(400, "password needs to contain at least one uppercase, one lowercase and one number")
+    # convert to a dict
+    result_data = result_user.as_dict()
 
-    # the email should be a valid email
-    email_pattern = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-    if not email_pattern.match(data["email"]):
-        abort(400, "invalid email")
+    if return_token:
+        # generate the token with jwt
+        if type == "student":
+            token = generate_token(result_user.student_id, type)
+        elif type == "supervisor":
+            token = generate_token(result_user.supervisor_id, type)
+        else:
+            token = generate_token(result_user.partner_id, type)
+
+        result_data["token"] = token
+        result_data["type"] = type
+
+    return result_data
+
 
 
 # assign a random avatar from the db folder, and save the relative path to the database
@@ -57,13 +73,6 @@ def get_random_avatar():
     return avatar_relative_path
 
 
-# define the api
-api = Namespace(
-    "auth",
-    description="login, register"
-)
-
-
 @api.route("/login")
 class Login(Resource):
     @api.doc(description="Login with username, type, password. All passwords are Abcd1234! by default database")
@@ -74,26 +83,15 @@ class Login(Resource):
     def post(self):
         """login with username + type + password"""
         data = request.get_json()
-        result_user = None
-
-        if data["type"] == 'student':
-            # filter by username and password
-            result_user = Student.query.filter_by(username=data["username"], password=data["password"]).first()
-        elif data["type"] == 'supervisor':
-            result_user = Supervisor.query.filter_by(username=data["username"], password=data["password"]).first()
-        else:
-            result_user = Partner.query.filter_by(username=data["username"], password=data["password"]).first()
-
-        if result_user is None:
+        result_data = login(username=data["username"], password=data["password"], type=data["type"], return_token=False)
+        if result_data is None:
             abort(401, "username or password is incorrect")
         else:
-            result_data = result_user.as_dict()
-            result_data["type"] = data["type"]
             return jsonify(result_data)
 
 
 @api.route("/login_with_email")
-class Login(Resource):
+class LoginWithEmail(Resource):
     @api.doc(description="Login with email + type + password. All passwords are Abcd1234! by default database")
     @api.expect(login_model_with_email)
     @api.response(200, 'success login, will return the user profile')
@@ -102,21 +100,44 @@ class Login(Resource):
     def post(self):
         """login with email + type + password"""
         data = request.get_json()
-        result_user = None
-
-        if data["type"] == 'student':
-            # filter by email and password
-            result_user = Student.query.filter_by(email=data["email"], password=data["password"]).first()
-        elif data["type"] == 'supervisor':
-            result_user = Supervisor.query.filter_by(email=data["email"], password=data["password"]).first()
-        else:
-            result_user = Partner.query.filter_by(email=data["email"], password=data["password"]).first()
-
-        if result_user is None:
+        result_data = login(email=data["email"], password=data["password"], type=data["type"], return_token=False)
+        if result_data is None:
             abort(401, "email or password is incorrect")
         else:
-            result_data = result_user.as_dict()
-            result_data["type"] = data["type"]
+            return jsonify(result_data)
+
+
+@api.route("/login_with_username_and_return_token")
+class LoginWithUsernameAndReturnToken(Resource):
+    @api.doc(description="Login with username + type + password. And return token with profile")
+    @api.expect(login_model)
+    @api.response(200, 'success login, will return the user profile + token')
+    @api.response(400, 'invalid username, password or type')
+    @api.response(401, 'valid inputs, but the user is not found, so login fail')
+    def post(self):
+        """login with username + type + password, return profile + token"""
+        data = request.get_json()
+        result_data = login(username=data["username"], password=data["password"], type=data["type"], return_token=True)
+        if result_data is None:
+            abort(401, "username or password is incorrect")
+        else:
+            return jsonify(result_data)
+
+
+@api.route("/login_with_email_and_return_token")
+class LoginWithEmailAndReturnToken(Resource):
+    @api.doc(description="Login with email + type + password. And return the token with profile")
+    @api.expect(login_model_with_email)
+    @api.response(200, 'success login, will return the user profile + token')
+    @api.response(400, 'invalid username, password or type')
+    @api.response(401, 'valid inputs, but the user is not found, so login fail')
+    def post(self):
+        """login with email + type + password, return profile + token"""
+        data = request.get_json()
+        result_data = login(email=data["email"], password=data["password"], type=data["type"], return_token=True)
+        if result_data is None:
+            abort(401, "email or password is incorrect")
+        else:
             return jsonify(result_data)
 
 
@@ -205,101 +226,3 @@ class Register(Resource):
             db.session.add(new_supervisor)
             db.session.commit()
             return jsonify(new_supervisor.as_dict())
-
-
-
-# @api.route("/student_register_no_resume")
-# class StudentRegisterWithoutResume(Resource):
-#     @api.doc(description="Student register, all fields are required, but no file is upload, so resume_url will be None")
-#     @api.response(200, 'success register, will return the user profile')
-#     @api.response(400, 'invalid inputs / username or email is duplicate')
-#     @api.expect(student_register_model)
-#     def post(self):
-#         # get the data from the request
-#         data = request.get_json()
-#         check_student_register_info(data)
-#         avatar_relative_path = get_random_avatar()
-
-#         # create a new student, without resume upload
-#         new_student = Student(
-#             username=data["username"],
-#             first_name=data["first_name"],
-#             last_name=data["last_name"],
-#             email=data["email"],
-#             password=data["password"],
-#             avatar=avatar_relative_path,
-#             qualification=data["qualification"],
-#             school_name=data["school_name"],
-#             major=data["major"],
-#             skills=data["skills"],
-#             strength=data["strength"],
-#             resume_url=None
-#         )
-
-#         db.session.add(new_student)
-#         db.session.commit()
-
-#         # return this student to the frontend
-#         return jsonify(new_student.as_dict())
-
-
-# @api.route("/student_register_with_resume")
-# class StudentRegisterWithResumeFile(Resource):
-#     @api.doc(description="Student register with resume in pdf, doc, docs, require file < 5MB, "
-#         + "The Content-Type is multipart/form-data, so the frontend need to use FormData to submit the file")
-#     @api.response(200, 'success register, will return the user profile')
-#     @api.response(400, 'invalid inputs / username or email is duplicate')
-#     @api.expect(student_register_with_resume_parser)
-#     def post(self):
-#         # get the data from the request
-#         data = request.form
-#         check_student_register_info(data)
-#         avatar_relative_path = get_random_avatar()
-
-#         # check this file exist
-#         if 'file' not in request.files:
-#             abort(400, 'require to submit a file')
-
-#         file = request.files['file']
-
-#         # require to match from the end of the file.filename: pdf, docx, doc
-#         if not file.filename.lower().endswith(('.pdf', '.docx', '.doc')):
-#             abort(400, 'require to submit a pdf, docx or doc file')
-
-#         # file cannot be too large, larger than 5MB will be rejected
-#         if len(file.read()) > 5 * 1024 * 1024:
-#             abort(400, 'file size cannot be larger than 5MB')
-
-#         # reset the file pointer to beginning
-#         file.seek(0)
-
-#         # no need to hash, simply add a timestamp at the beginning
-#         # and replace \\ to /
-#         new_filename = "{}-{}".format(int(time.time()), file.filename)
-#         new_full_filename = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
-#         new_full_filename = new_full_filename.replace("\\", "/")
-
-#         # save the file
-#         file.save(new_full_filename)
-
-#         # create a new student, without resume upload
-#         new_student = Student(
-#             username=data["username"],
-#             first_name=data["first_name"],
-#             last_name=data["last_name"],
-#             email=data["email"],
-#             password=data["password"],
-#             avatar=avatar_relative_path,
-#             qualification=data["qualification"],
-#             school_name=data["school_name"],
-#             major=data["major"],
-#             skills=data["skills"],
-#             strength=data["strength"],
-#             resume_url=new_full_filename
-#         )
-
-#         db.session.add(new_student)
-#         db.session.commit()
-
-#         # return this student to the frontend
-#         return jsonify(new_student.as_dict())
--- backend/apis/util.py
+++ backend/apis/util.py
@@ -1,5 +1,45 @@
 import hashlib
 import uuid
+import jwt
+from db.models import *
+
+SECRET_KEY = "secret%^&AMGASDAS!!@#!@#1112"
+
+# generate a token for the given user_id and type
+def generate_token(user_id, type):
+    data = {
+        "user_id": user_id,
+        "type": type
+    }
+
+    token = jwt.encode(data, SECRET_KEY, algorithm="HS256")
+    return token
+
+
+# decode the given token and return the user object
+# the user object can be either a student, supervisor or partner
+def decode_token_and_get_user_object(token):
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    except Exception:
+        return None
+
+    # now use the user_id and type to obtain this user
+    user_id = data["user_id"]
+    type = data["type"]
+
+    if type == "stduent":
+        student = Student.query.filter_by(student_id=user_id).first()
+        return student
+    elif type == "supervisor":
+        supervisor = Supervisor.query.filter_by(supervisor_id=user_id).first()
+        return supervisor
+    elif type == "partner":
+        partner = Partner.query.filter_by(partner_id=user_id).first()
+        return partner
+    else:
+        return None
+
 
 # hash the given file, keep the original extension
 # reference: https://www.programiz.com/python-programming/examples/hash-file
