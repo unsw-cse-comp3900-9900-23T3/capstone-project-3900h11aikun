e5dff3d49bd767ddf61fa16199dcb48a2d5921fc
Doris <doris@DorisdeMacBook-Air.local>
2023-11-01 17:06:39 +1100

    finish update profile exclude upload resume

--- frontend/stSuInfo/stSuinfo.js
+++ frontend/stSuInfo/stSuinfo.js
@@ -1,10 +1,17 @@
+import { doFetch } from "../helper.js";
+
 const home = document.getElementById('home');
 const myPro = document.getElementById('mypro');
 const profile = document.getElementById('profile');
 const logout = document.getElementById('logout');
 const ret = document.getElementById('return');
 const view = document.getElementById('view');
+const skill = document.getElementById('skill');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const strength = document.getElementById('strength');
 const download = document.getElementById('download');
+const stOrSu = document.getElementById('stOrSu');
 
 
 // Interaction display
@@ -25,6 +32,9 @@ ret.addEventListener('mouseleave', (event) => {
     ret.style.background = 'white';
     ret.style.color = 'black';
 });
+ret.addEventListener('click', (event) => {
+    window.location.href = "../HomeDir/home.html";
+});
 
 function buttonDisplay (item) {
     item.addEventListener('mouseover', (event) => {
@@ -41,7 +51,17 @@ naviDisplay(profile);
 naviDisplay(logout);
 buttonDisplay(view);
 buttonDisplay(download);
-
-
+const student_id = localStorage.getItem('roleId');
+console.log(`/studentInfo/getStudentInfo/{${student_id}}`);
+if (localStorage.getItem('role') === "supervisor") {
+    stOrSu = "Supervisor Information";
+}
+doFetch(`/studentInfo/getStudentInfo/${student_id}`, 'GET').then((data)=>{
+    console.log(data);
+    skill.textContent = data.skills;
+    name.textContent = data.first_name + ' ' + data.last_name;
+    email.textContent = data.email;
+    strength.textContent = data.strength;
+})
 
 
